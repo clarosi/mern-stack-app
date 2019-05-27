0d460d6bc34a1f44d5dcc6159c80fe4943c16bd2
Claros <ian.r.claros@accenture.com>
2019-05-27 16:44:19 +0800

    05/27/2019 commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
